8de989bb677b28999caf4ff7c836feaff75100d2
colderry <c0l4e6r7@gmail.com>
2023-04-27 05:50:46 +0600

    feat: inital setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Henlo World");
+const { stdin: input, stdout: output } = require("node:process");
+const { version } = require("../package.json");
+const readline = require("node:readline");
+
+const r = readline.createInterface({ input, output });
+const prefix = "!";
+
+console.log(`Copenhagent v${version}`);
+console.log(`Type "${prefix}help" for more information.`);
+
+r.on("line", (data) => {
+    const args = data.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    switch (cmd) {
+        case "help": require("./commands/help").exec({ prefix, args });
+        break;
+        case "lookup": require("./commands/lookup").exec({ prefix, args });
+        break;
+        default: console.error(`Command: "${cmd}" does not exist.`);
+    }
+});
